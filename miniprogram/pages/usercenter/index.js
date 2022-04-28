97cf63a374841cf5476e343afc64dc97dd4ae8d5
hchen <chen.huang@bst.ai>
2022-04-28 15:42:57 +0800

    new start

---
+++ miniprogram/pages/usercenter/index.js
@@ -0,0 +1,161 @@
+const menuData = [
+  [{
+      title: '收货地址',
+      tit: '',
+      url: '',
+      type: 'address',
+    },
+    {
+      title: '优惠券',
+      tit: '',
+      url: '',
+      type: 'coupon',
+    },
+    {
+      title: '积分',
+      tit: '',
+      url: '',
+      type: 'point',
+    },
+  ],
+  [{
+      title: '帮助中心',
+      tit: '',
+      url: '',
+      type: 'help-center',
+    },
+    {
+      title: '客服热线',
+      tit: '',
+      url: '',
+      type: 'service',
+      icon: 'service',
+    },
+  ],
+];
+
+const orderTagInfos = [{
+    title: '待付款',
+    iconName: 'wallet',
+    orderNum: 0,
+    tabType: 5,
+    status: 1,
+  },
+  {
+    title: '待发货',
+    iconName: 'deliver',
+    orderNum: 0,
+    tabType: 10,
+    status: 1,
+  },
+  {
+    title: '待收货',
+    iconName: 'package',
+    orderNum: 0,
+    tabType: 40,
+    status: 1,
+  },
+  {
+    title: '待评价',
+    iconName: 'comment',
+    orderNum: 0,
+    tabType: 60,
+    status: 1,
+  },
+  {
+    title: '退款/售后',
+    iconName: 'exchang',
+    orderNum: 0,
+    tabType: 0,
+    status: 1,
+  },
+];
+
+const getDefaultData = () => ({
+  showMakePhone: false,
+  userInfo: {
+    avatarUrl: '',
+    nickName: '正在登录...',
+    phoneNumber: '',
+  },
+  menuData,
+  orderTagInfos,
+  customerServiceInfo: {},
+  currAuthStep: 1,
+  showKefu: true,
+  versionNo: '1.0',
+});
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: getDefaultData(),
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  },
+
+  gotoUserEditPage() {
+    const {
+      currAuthStep
+    } = this.data;
+
+    if (currAuthStep === 1) {
+
+    }
+  }
+})
