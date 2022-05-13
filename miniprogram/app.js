b5a79bb99327b001b4e0b97c415fb6b3c240161f
hchen <chen.huang@bst.ai>
2022-05-13 10:03:23 +0800

    fully restart

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,21 +1,2 @@
 // app.js
-App({
-  onLaunch: function () {
-    if (!wx.cloud) {
-      console.error('版本过低')
-    } else {
-      wx.cloud.init({
-        traceUser: true
-      })
-    }
-  },
-  globalData: {
-    isLogin: false,
-    user: {
-      avatar: '',
-      nickname: '',
-      phone: ''
-    },
-    openID: ''
-  }
-})
+App({})
